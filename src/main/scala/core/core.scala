da1c09804dec6f94a8051b4974a67088e65e665a
Sasha Freyja Zurek <sasha@sashafreyjazurek.xyz>
2022-03-18 17:31:06 -0400

    ADDI and SUBI implemented

--- src/main/scala/core/core.scala
+++ src/main/scala/core/core.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 import chisel3.stage.ChiselStage
 
 class WildcatCore() extends Module {
-    /* outline
+  /* outline
         so, i'm not really sure how best to lay this out, but i'm getting something down
         so i don't feel bad about how little i've written.
 
@@ -21,85 +21,116 @@ class WildcatCore() extends Module {
 
         i think the following order for building the core would likely work out:
             >get decode logic working first (this is necessary for everything else)
-                * make sure to have a test harness which tests each of the 20 instructions
+   * make sure to have a test harness which tests each of the 20 instructions
             >after decode logic works, implement the simple instructions
             >then implement conditional logic
             >then implement skip logic
-                * here is where the fibonnaci sequence should work
-                * this also requires the program counter
+   * here is where the fibonnaci sequence should work
+   * this also requires the program counter
             >implement a memory
             >implement load/store
             >working CPU?
-    */
-   val io = IO(new Bundle {
-     /* I wonder if we can implement PC through this */
-        val boot = Input(Bool())
-        val is_write = Input(Bool())
-        val write_addr = Input(UInt(24.W))
-        val write_data = Input(UInt(24.W))
-        val valid = Output(Bool())
-        val inst = Output(UInt(24.W))
-        /* debug outputs */
-        val pc = Output(UInt(14.W))
-        val imm = Output(UInt(14.W))
-        val src = Output(UInt(5.W))
-        val op = Output(UInt(5.W))
-   })
-   val pc = RegInit(0.U(14.W)) /* 2^14 is maximum size of mem */
-   val cond_pass = Reg(Bool()) /* one flag, for "previous cond pass", i guess */
-   /* adding these as stub until needed
+   */
+  val io = IO(new Bundle {
+    val boot = Input(Bool())
+    val is_write = Input(Bool())
+    val write_addr = Input(UInt(24.W))
+    val write_data = Input(UInt(24.W))
+    val valid = Output(Bool())
+    val inst = Output(UInt(24.W))
+    /* debug outputs */
+    val pc = Output(UInt(14.W))
+    val imm = Output(UInt(14.W))
+    val src = Output(UInt(5.W))
+    val op = Output(UInt(5.W))
+    val result = Output(UInt(24.W)) // output of last ins
+  })
+  val reg = Mem(32, UInt(24.W))
+  reg(0) := 0.U
+  val pc = RegInit(0.U(14.W)) /* 2^14 is maximum size of mem */
+  val cond_pass = Reg(Bool()) /* one flag, for "previous cond pass", i guess */
+  /* adding these as stub until needed
    val working_mem = Mem()
    */
-   val program_mem = Mem(256, UInt(24.W))
+  val program_mem = Mem(256, UInt(24.W))
 
-   val inst = program_mem(pc)
-   val imm = inst(23,10)
-   val src = inst(9,5)
-   val op = inst(4,0)
-   // here might eventually be a "write to memory" section
-   // we're not worrying about that now
-   when (io.is_write) {
-        program_mem(io.write_addr) := io.write_data
-        io.valid := true.B
-   } .elsewhen (io.boot) {
-        pc := 0.U
-        io.valid := true.B
-   } .otherwise {
-        when (op > 19.U) {
-            io.valid := false.B
-        } .otherwise {
-           /* decode instruction */
-            io.valid := true.B
-            switch(op) {
-                is (Opcode.add) {}
-                is (Opcode.addi) {}
-                is (Opcode.sub) {}
-                is (Opcode.subi) {}
-                is (Opcode.mul) {}
-                is (Opcode.div) {}
-                is (Opcode.mod) {}
-                is (Opcode.and) {}
-                is (Opcode.or) {}
-                is (Opcode.xor) {}
-                is (Opcode.not) {}
-                is (Opcode.gre) {}
-                is (Opcode.les) {}
-                is (Opcode.equ) {}
-                is (Opcode.mov) {}
-                is (Opcode.ldi) {}
-                is (Opcode.sto) {}
-                is (Opcode.ld) {}
-                is (Opcode.jmp) {}
-                is (Opcode.skp) {}
+  val inst = program_mem(pc)
+  val imm = inst(23, 10)
+  val src = inst(9, 5)
+  val op = inst(4, 0)
+  io.result := 127.U
+  // here might eventually be a "write to memory" section
+  // we're not worrying about that now
+  when(io.is_write) {
+    program_mem(io.write_addr) := io.write_data
+    io.valid := true.B
+  }.elsewhen(io.boot) {
+    pc := 0.U
+    io.valid := true.B
+  }.otherwise {
+    when(op > 19.U) {
+      io.valid := false.B
+      // R0 should never be modified by spec
+      // unsure how to implement
+    }.otherwise {
+      /* decode instruction */
+      io.valid := true.B
+      /* register-immediate */
+      when(op > 12.U) {
+        switch(op) {
+          is(Opcode.addi) {
+            when(src > 0.U) {
+              val res = reg(src) + imm
+              reg(src) := res
+              io.result := res
+            }.otherwise {
+              io.valid := false.B
             }
+          }
+          is(Opcode.subi) {
+            when(src > 0.U) {
+              val res = reg(src) - imm
+              reg(src) := res
+              io.result := res
+            }.otherwise {
+              io.valid := false.B
+            }
+          }
+          is(Opcode.ldi) {}
+          is(Opcode.sto) {}
+          is(Opcode.ld) {}
+          is(Opcode.jmp) {}
+          is(Opcode.skp) {}
         }
-        pc := pc + 1.U
-   } 
-   io.inst := inst 
-   io.pc := pc
-   io.imm := imm
-   io.src := src
-   io.op := op
+        /* register-register
+      imm is destination register
+         */
+      }.otherwise {
+        // check write register for 0
+        switch(op) {
+          is(Opcode.add) {}
+          is(Opcode.sub) {}
+          is(Opcode.mul) {}
+          is(Opcode.div) {}
+          is(Opcode.mod) {}
+          is(Opcode.and) {}
+          is(Opcode.or) {}
+          is(Opcode.xor) {}
+          is(Opcode.not) {}
+          is(Opcode.gre) {}
+          is(Opcode.les) {}
+          is(Opcode.equ) {}
+          is(Opcode.mov) {}
+        }
+      }
+    }
+    pc := pc + 1.U
+  }
+  /* filling out debug outputs */
+  io.inst := inst
+  io.pc := pc
+  io.imm := imm
+  io.src := src
+  io.op := op
 
-      
 }
--- src/test/scala/core/coreTests.scala
+++ src/test/scala/core/coreTests.scala
@@ -6,99 +6,124 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CoreTest extends AnyFlatSpec with ChiselScalatestTester {
-    /*def instruction(op: UInt, dest: UInt, src: UInt, imm: UInt) = {
+  /*def instruction(op: UInt, dest: UInt, src: UInt, imm: UInt) = {
         if (op.litValue > 13) {
             Cat(imm, src, op)
         } else {
             Cat(dest, src, op)
         }
     }*/
-    def write_ins(c: WildcatCore, addr: UInt, code: UInt) = {
-        c.io.is_write.poke(true.B)
-        c.io.boot.poke(false.B)
-        c.io.write_addr.poke(addr)
-        c.io.write_data.poke(code)
-        c.clock.step(1)
+  def write_ins(c: WildcatCore, addr: UInt, code: UInt) = {
+    c.io.is_write.poke(true.B)
+    c.io.boot.poke(false.B)
+    c.io.write_addr.poke(addr)
+    c.io.write_data.poke(code)
+    c.clock.step(1)
+  }
+  def boot(c: WildcatCore) = {
+    c.io.is_write.poke(false.B)
+    c.io.boot.poke(true.B)
+    c.clock.step(1)
+  }
+  behavior of "WildcatCore"
+  it should "test writing to memory" in {
+    test(new WildcatCore()) { c =>
+      // write one instruction
+      write_ins(c, 0.U, 13056.U)
+      // do that instruction
+      // partially tests decoding, too
+      c.io.is_write.poke(false.B)
+      c.io.boot.poke(true.B)
+      c.io.inst.expect(13056.U)
+      c.io.valid.expect(true.B)
     }
-    def boot(c: WildcatCore) = {
-        c.io.is_write.poke(false.B)
-        c.io.boot.poke(true.B)
+  }
+  it should "test instruction decoding" in {
+    test(new WildcatCore()) { c =>
+      //  add
+      write_ins(c, 0.U, 13056.U)
+      //  addi
+      write_ins(c, 1.U, 13069.U)
+      //  sub
+      write_ins(c, 2.U, 13057.U)
+      //  subi
+      write_ins(c, 3.U, 13070.U)
+      //  mul
+      write_ins(c, 4.U, 13058.U)
+      //  div
+      write_ins(c, 5.U, 13059.U)
+      //  mod
+      write_ins(c, 6.U, 13060.U)
+      //  mov
+      write_ins(c, 7.U, 13061.U)
+      //  ldi
+      write_ins(c, 8.U, 13071.U)
+      //  sto
+      write_ins(c, 9.U, 13072.U)
+      //  ld
+      write_ins(c, 10.U, 13073.U)
+      //  and
+      write_ins(c, 11.U, 13062.U)
+      //  or
+      write_ins(c, 12.U, 13063.U)
+      //  xor
+      write_ins(c, 13.U, 13064.U)
+      //  not
+      write_ins(c, 14.U, 13065.U)
+      //  gre
+      write_ins(c, 15.U, 13066.U)
+      //  les
+      write_ins(c, 16.U, 13067.U)
+      //  equ
+      write_ins(c, 17.U, 13068.U)
+      //  jmp
+      write_ins(c, 18.U, 13074.U)
+      //  skp
+      write_ins(c, 19.U, 13075.U)
+      //  invalid
+      write_ins(c, 20.U, 13076.U)
+
+      // boot the CPU
+      boot(c)
+      c.io.boot.poke(false.B)
+      // first 19 instructions should be valid
+      for (ins <- 0 until 20) {
+        println(c.io.pc.peek())
+        println(c.io.inst.peek())
+        c.io.valid.expect(true.B)
+        c.io.pc.expect(ins.U)
         c.clock.step(1)
+      }
+      // last should be invalid
+      c.io.valid.expect(false.B)
+      c.io.pc.expect(20.U)
     }
-    behavior of "WildcatCore"
-        it should "test writing to memory" in {
-/* try putting "write instruction" in a funciton again tomorrow
-by putting c: WildcatCore */
-            test(new WildcatCore()) { c =>
-                // write one instruction
-                write_ins(c,0.U,13056.U)
-                // do that instruction
-                // partially tests decoding, too
-                c.io.is_write.poke(false.B)
-                c.io.boot.poke(true.B)
-                c.io.inst.expect(13056.U)
-                c.io.valid.expect(true.B)
-            }
-        }
-        it should "test instruction decoding" in {
-            test(new WildcatCore()) { c =>
-                //  add
-                write_ins(c,0.U,13056.U)
-                //  addi
-                write_ins(c,1.U,13069.U)
-                //  sub
-                write_ins(c,2.U,13057.U)
-                //  subi
-                write_ins(c,3.U,13070.U)
-                //  mul
-                write_ins(c,4.U,13058.U)
-                //  div
-                write_ins(c,5.U,13059.U)
-                //  mod
-                write_ins(c,6.U,13060.U)
-                //  mov
-                write_ins(c,7.U,13061.U)
-                //  ldi
-                write_ins(c,8.U,13071.U)
-                //  sto
-                write_ins(c,9.U,13072.U)
-                //  ld
-                write_ins(c,10.U,13073.U)
-                //  and
-                write_ins(c,11.U,13062.U)
-                //  or
-                write_ins(c,12.U,13063.U)
-                //  xor
-                write_ins(c,13.U,13064.U)
-                //  not
-                write_ins(c,14.U,13065.U)
-                //  gre
-                write_ins(c,15.U,13066.U)
-                //  les
-                write_ins(c,16.U,13067.U)
-                //  equ
-                write_ins(c,17.U,13068.U)
-                //  jmp
-                write_ins(c,18.U,13074.U)
-                //  skp
-                write_ins(c,19.U,13075.U)
-                //  invalid
-                write_ins(c,20.U,13076.U)
-                
-                // boot the CPU
-                boot(c) 
-                c.io.boot.poke(false.B)
-                // first 19 instructions should be valid
-                for (ins <- 0 until 20) {
-                    println(c.io.pc.peek())
-                    println(c.io.inst.peek())
-                    c.io.valid.expect(true.B)
-                    c.io.pc.expect(ins.U)
-                    c.clock.step(1)
-                }
-                // last should be invalid
-                c.io.valid.expect(false.B)
-                c.io.pc.expect(20.U)
-            }
-        }
+  }
+  it should "test imm arithmetic instructions" in {
+    test(new WildcatCore()) { c =>
+      write_ins(c, 0.U, 3117.U) // ADDI r1 0h3
+      write_ins(c, 1.U, 3118.U) // SUBI r1 0h3
+      write_ins(c, 2.U, 3085.U) // ADDI r0 0h3
+      write_ins(c, 3.U, 3086.U) // SUBI r0 0h3
+      boot(c)
+      c.io.boot.poke(false.B)
+      // test ADDI
+      c.io.valid.expect(true.B)
+      c.io.op.expect(13.U)
+      c.io.result.expect(3.U)
+      c.clock.step(1)
+
+      // test SUBI
+      c.io.valid.expect(true.B)
+      c.io.op.expect(14.U)
+      c.io.result.expect(0.U)
+      c.clock.step(1)
+
+      // test fail on 0 regs
+      c.io.valid.expect(false.B)
+      c.clock.step(1)
+      c.io.valid.expect(false.B)
+      c.clock.step(1)
+    }
+  }
 }
